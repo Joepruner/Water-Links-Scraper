6b9f0de501ef10071b9c2fb5551f9ba9988c6e0a
Joseph Pruner <joepruner@gmail.com>
2019-04-25 09:56:57 -0700

    minor optimizations

--- run_my_spiders.py
+++ run_my_spiders.py
@@ -6,28 +6,9 @@ from scrapy.utils.project import get_project_settings
 from multiprocessing import Process
 import time
 
-
-
-
-
-
-# timeout = time.time() + 3
-
 crawler_process = CrawlerProcess(get_project_settings())
 crawler_process.crawl(WaterlinksSpider)
 node_filler_multi_process = Process(target=fn.fill_nodes)
 
 node_filler_multi_process.start()
 crawler_process.start()
-# spider_1_process = Process(target=crawler_process.start())
-# crawler_process.start()
-# while True:
-#     if time.time() > timeout:
-#         print("*************STOPPING**************")
-#         crawler_process.stop()
-#         node_filler_process.start()
-#         break
-
-
-
-
